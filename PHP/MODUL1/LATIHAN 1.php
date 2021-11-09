280b9f0321badfac97e6433d78cb861214137ac5
Revanza-Refnaldy <93989065+Revanza-Refnaldy@users.noreply.github.com>
2021-11-09 20:38:40 +0700

    Add files via upload

---
+++ PHP/MODUL1/LATIHAN 1.php
@@ -0,0 +1,5 @@
+<?php
+    echo "Namaku Revanza";
+    echo "<br>";
+    echo "Aku Sedang Belajar PHP di SMK Telkom Malang";
+?>
---
+++ PHP/MODUL1/MENAMPILKAN VARIABEL DENGAN ECHO.php
@@ -0,0 +1,21 @@
+<?php
+    
+    //variable
+    $nama = "Firdausa";
+    $sekolah = "SMK Telkom Malang";
+    $umur = 16;
+    
+    //menampilkan nilai variabel
+
+    //contoh 1
+    echo "Halo namaku $nama usiaku $umur tahun";
+    echo "<br>";
+    echo "Aku sekolah di $sekolah";
+    
+    //contoh 2
+    echo 'Halo namaku '.$nama.' usiaku '.$umur.' tahun';
+    
+    echo "<br>";
+    echo 'Aku sekolah di '.$sekolah;
+?>
+
